eb4a2ebf0ea110059afe924e809f92d359ccf661
Mateusz Dereniowski <derenio@gmail.com>
2013-09-06 18:50:23 +0200

    Update deploy.sh

--- cmscloud_client/resources/deploy.sh
+++ cmscloud_client/resources/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-WORKSPACE_FOLDER="/Users/christianbertschy/Documents/workspace"
+WORKSPACE_FOLDER="$HOME/Documents/workspace"
 PYINSTALLER_FOLDER="$WORKSPACE_FOLDER/pyinstaller"
 SEARCH_FOLDER="$WORKSPACE_FOLDER/cmscloud-client/cmscloud_client"
 PACKAGING_FOLDER="$WORKSPACE_FOLDER/packages"
 MAIN_PYTHON_SCRIPT="$WORKSPACE_FOLDER/cmscloud-client/cmscloud_client/main.py"
 CONTENTS_FOLDER="$WORKSPACE_FOLDER/cmscloud-client/cmscloud_client/resources"
+ICON_FILE="$CONTENTS_FOLDER/appIcon.icns"
 
 DEPLOY_NAME="AldrynCloud"
 DIST_FOLDER="$PYINSTALLER_FOLDER/$DEPLOY_NAME/dist"
@@ -15,13 +16,13 @@ rm -rf $PACKAGING_FOLDER/$DEPLOY_NAME.dmg
 
 cd $PYINSTALLER_FOLDER
 rm -rf $DEPLOY_NAME
-python pyinstaller.py -n $DEPLOY_NAME $MAIN_PYTHON_SCRIPT
+python pyinstaller.py --windowed -n $DEPLOY_NAME $MAIN_PYTHON_SCRIPT
 cd $DEPLOY_NAME
 echo 'install_hooks(globals())' | cat - "$DEPLOY_NAME.spec" > temp && mv temp "$DEPLOY_NAME.spec"
 echo 'from kivy.tools.packaging.pyinstaller_hooks import install_hooks' | cat - "$DEPLOY_NAME.spec" > temp && mv temp "$DEPLOY_NAME.spec"
 
 #set hiddenimports
-perl -i -pe 'BEGIN{undef $/;} s/hiddenimports=\[\],/hiddenimports=\['\"setuptools\"', '\"distutils\"', '\"cython\"', '\"requests\"', '\"watchdog\"', '\"certifi\"'],/smg' "$DEPLOY_NAME.spec"
+perl -i -pe 'BEGIN{undef $/;} s/hiddenimports=\[\],/hiddenimports=\['\"setuptools\"', '\"distutils\"', '\"cython\"', '\"requests\"', '\"watchdog\"', '\"certifi\"', '\"kivy.core.image.img_gif\"'],/smg' "$DEPLOY_NAME.spec"
 
 #set hookspath to none, otherwise kivy breaks
 perl -i -pe 'BEGIN{undef $/;} s/,\n\s+hookspath=None//smg' "$DEPLOY_NAME.spec"
@@ -35,23 +36,19 @@ perl -ni -e 'print; print "a.datas.append((\"cacert.pem\", \"cacert.pem\", \"DAT
 SEARCH_FOLDER=$(echo $SEARCH_FOLDER|sed "s/\\//\\\\\//g")
 perl -i -pe 'BEGIN{undef $/;} s/COLLECT\(exe,/COLLECT\(exe, Tree\('\"$SEARCH_FOLDER\"'\),/smg' "$DEPLOY_NAME.spec"
 
+#set application icon
+ICON_FILE=$(echo $ICON_FILE|sed "s/\\//\\\\\//g")
+perl -i -pe 'BEGIN{undef $/;} s/BUNDLE\(coll,/BUNDLE\(coll, icon='\"$ICON_FILE\"',/smg' "$DEPLOY_NAME.spec"
+
 cd ..
 rm -rf "./$DEPLOY_NAME/dist/$DEPLOY_NAME"
-python pyinstaller.py "./$DEPLOY_NAME/$DEPLOY_NAME.spec"
-
-cd $DIST_FOLDER
-mv "$DEPLOY_NAME" "$DEPLOY_NAME.app"
-
-
-#Add plist and icon manually, not yet supported by pyinstaller automatically
-cd $CONTENTS_FOLDER
-cp "appIcon.icns" "$DIST_FOLDER/$DEPLOY_NAME.app/"
-cp "info.plist" "$DIST_FOLDER/$DEPLOY_NAME.app/"
+rm -rf "./$DEPLOY_NAME/dist/$DEPLOY_NAME.app"
+kivy pyinstaller.py "./$DEPLOY_NAME/$DEPLOY_NAME.spec"
 
 #Create DMG
 pushd $DIST_FOLDER
 hdiutil create ./$DEPLOY_NAME.dmg -srcfolder $DEPLOY_NAME.app -ov
-popd 
+popd
 
 #Move created files to packaging folder
 mv -f $DIST_FOLDER/$DEPLOY_NAME.app $PACKAGING_FOLDER/
@@ -63,4 +60,3 @@ rm -rf $DEPLOY_NAME
 cd $PACKAGING_FOLDER
 cd ..
 open $PACKAGING_FOLDER
- 
