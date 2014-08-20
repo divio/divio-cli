49ff3c4279901647d285b2a8d177b46563200473
Suriyan Laohaprapanon <suriyant@gmail.com>
2014-08-20 14:09:09 +0700

    Update deploy-linux.sh

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -3,8 +3,10 @@
 echo '***********************************'
 echo 'Aldryn Linux deployment script     '
 echo '***********************************'
-sudo python setup.py install
 rm -rf ./build
+mkdir -p ./build
+mkdir -p ./dist
+sudo python setup.py install
 rm -f ./aldryn_client/aldryngui.ini
 rm -rf ./dist/Aldryn
 rm -rf ./dist/Aldryn.bin
