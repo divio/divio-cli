02cb4df50ad43f25e85179d26c083ba23d3e3710
Kim Thoenen <kim@smuzey.ch>
2016-02-22 17:40:02 +0100

    improve check system command
    - support for machine readable format
    - support for running individual checks

--- aldryn_client/check_system.py
+++ aldryn_client/check_system.py
@@ -1,54 +1,67 @@
 # -*- coding: utf-8 -*-
 import os
 import subprocess
+from collections import OrderedDict
 
 import click
 
 from . import utils
 
+ALL_CHECKS = OrderedDict([
+    ('git', ('Git', ['git', '--version'])),
+    ('docker-client', ('Docker Client', ['docker', '--version'])),
+    ('docker-machine', ('Docker Machine', ['docker-machine', '--version'])),
+    ('docker-compose', ('Docker Compose', ['docker-compose', '--version'])),
+    ('docker-server', ('Docker Server Connectivity', ['docker', 'ps'])),
+])
 
-def check_requirements(silent=False):
-    checks = [
-        ('git client', ['git', '--version']),
-        ('docker client', ['docker', '--version']),
-        ('docker server connection', ['docker', 'ps']),
-        ('docker-compose', ['docker-compose', '--version']),
-    ]
 
+def check_command(command):
+    error_msg = None
+    try:
+        utils.check_call(command, catch=False, silent=True)
+    except OSError as exc:
+        if exc.errno == os.errno.ENOENT:
+            error_msg = 'executable {} not found'.format(command[0])
+        else:
+            error_msg = (
+                'unknown error while trying to run {}: {}'
+                .format(command, exc.message)
+            )
+    except subprocess.CalledProcessError as exc:
+        error_msg = exc.output or str(exc)
+    return error_msg
+
+
+def check_requirements(checks=None):
+    if checks is None:
+        checks = ALL_CHECKS.keys()
+
+    for check in checks:
+        check_name, cmd = ALL_CHECKS[check]
+        yield check, check_name, check_command(cmd)
+
+
+def check_requirements_human(checks=None, silent=False):
     errors = []
 
-    for check, cmd in checks:
-        error_msg = None
-        try:
-            utils.check_call(cmd, catch=False, silent=True)
-        except OSError as exc:
-            if exc.errno == os.errno.ENOENT:
-                error_msg = 'executable {} not found'.format(cmd[0])
+    for check, check_name, error in check_requirements(checks):
+        if error:
+            errors.append((check_name, error))
+        if not silent:
+            is_windows = utils.is_windows()
+            if error:
+                symbol = ' ERR ' if is_windows else ' ✖  '
+                color = 'red'
             else:
-                error_msg = (
-                    'unknown error while trying to run {}: {}'
-                    .format(cmd, exc.message)
-                )
-        except subprocess.CalledProcessError as exc:
-            error_msg = exc.output or str(exc)
-        finally:
-            if error_msg:
-                errors.append((check, error_msg))
-
-            if not silent:
-                is_windows = utils.is_windows()
-                if error_msg:
-                    symbol = ' ERR ' if is_windows else ' ✖  '
-                    color = 'red'
-                else:
-                    symbol = ' OK  ' if is_windows else ' ✓  '
-                    color = 'green'
-                click.secho(symbol, fg=color, nl=False)
-                click.secho('{}'.format(check))
+                symbol = ' OK  ' if is_windows else ' ✓  '
+                color = 'green'
+            click.secho(symbol, fg=color, nl=False)
+            click.secho(check_name)
 
     if errors and not silent:
-        click.secho('\nThe following errors happened:', fg='red')
-        for error, msg in errors:
-            click.secho(' {}:\n > {}'.format(error, msg))
+        click.secho('\nThe following errors occurred:', fg='red')
+        for check_name, msg in errors:
+            click.secho(' {}:\n > {}'.format(check_name, msg))
 
     return not bool(errors)
