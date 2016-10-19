divio/divio-cli

commit f6f64bc299f4369047de62c13eb428c4ea80478f

local database import and export

aldryn_client/localdev/utils.py
@@ -1,10 +1,12 @@
 import json
 import sys
 import os
+from time import time
 
 import click
 
-from ..utils import check_output, is_windows
+from ..utils import check_output, is_windows, check_call
+from .. import exceptions
 from .. import settings
 
 
@@ -134,5 +136,15 @@ def ensure_windows_docker_compose_file_exists(path):
 
 def get_db_container_id(path):
     docker_compose = get_docker_compose_cmd(path)
-    output = check_output(docker_compose('ps', '-q', 'db'))
-    return output.rstrip(os.linesep)
+    output = check_output(docker_compose('ps', '-q', 'db')).rstrip(os.linesep)
+    if not output:
+        raise exceptions.AldrynException('Unable to find database container')
+    return output
+
+
+def start_database_server(docker_compose):
+    start_db = time()
+    click.secho(' ---> Starting local database server')
+    click.secho('      ', nl=False)
+    check_call(docker_compose('up', '-d', 'db'))
+    click.secho('      [{}s]'.format(int(time() - start_db)))

aldryn_client/cli.py
@@ -275,7 +275,7 @@ def pull_db(obj, stage):
     Pull database from your deployed website. Stage is either
     test (default) or live
     """
-    localdev.pull_db(obj, stage)
+    localdev.ImportRemoteDatabase(client=obj, stage=stage)()
 
 
 @project_pull.command(name='media')
@@ -359,6 +359,36 @@ def push_media(obj, stage):
     if not click.confirm('\nAre you sure you want to continue?'):
         return
     localdev.push_media(obj, stage)
+
+
+@project.group(name='import')
+def project_import():
+    """Import local database dump"""
+    pass
+
+
+@project_import.command(name='db')
+@click.argument('dump-path', default=localdev.DEFAULT_DUMP_FILENAME, type=click.Path(exists=True))
+@click.pass_obj
+def import_db(obj, dump_path):
+    """
+    Load a database dump into your local database
+    """
+    localdev.ImportLocalDatabase(client=obj, custom_dump_path=dump_path)()
+
+
+@project.group(name='export')
+def project_export():
+    """Export local database dump"""
+    pass
+
+
+@project_export.command(name='db')
+def export_db():
+    """
+    Export a dump of your local database
+    """
+    localdev.export_db()
 
 
 @project.command(name='develop')

commit e55c85a29ea450114653ab28ca330581bdc2b6ea

fix setup error if db could not be found
introduced in https://github.com/aldryn/aldryn-client/commit/f6f64bc299f4369047de62c13eb428c4ea80478f#diff-590ac9635c8048cc2b0e525dde6920bbR141

aldryn_client/localdev/main.py
@@ -68,7 +68,7 @@ def configure_project(website_slug, path, client):
 def setup_website_containers(client, stage, path):
     docker_compose = utils.get_docker_compose_cmd(path)
 
-    existing_db_container_id = utils.get_db_container_id(path)
+    existing_db_container_id = utils.get_db_container_id(path, False)
 
     # stop all running for project
     check_call(docker_compose('stop'))

aldryn_client/localdev/utils.py
@@ -134,10 +134,10 @@ def ensure_windows_docker_compose_file_exists(path):
         yaml.safe_dump(config, fh)
 
 
-def get_db_container_id(path):
+def get_db_container_id(path, raise_on_missing=True):
     docker_compose = get_docker_compose_cmd(path)
     output = check_output(docker_compose('ps', '-q', 'db')).rstrip(os.linesep)
-    if not output:
+    if not output and raise_on_missing:
         raise exceptions.AldrynException('Unable to find database container')
     return output
 