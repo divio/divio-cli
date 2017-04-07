0cf0c134f5fb9ccb32ca41ee76be03bd7171b944
Stefan Foulis <stefan@foulis.ch>
2017-04-07 10:54:24 +0200

    Automated Windows build
    Run tests and build on appveyor.

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -15,8 +15,12 @@ C:\Python27\Scripts\pip.exe install --upgrade pip
 C:\Python27\Scripts\pip.exe install virtualenv
 C:\Python27\Scripts\virtualenv.exe $VENV
 
+# pip and pyinstaller generate lots of warnings, so we need to ignore them
+$ErrorActionPreference = "Continue"
+
 # install build requirements
-Invoke-Expression ".\$VENV\Scripts\pip.exe install -r requirements.txt -r requirements-windows.txt -r requirements-build.txt"
+Invoke-Expression ".\$VENV\Scripts\pip.exe install --allow-external pyinstaller -r requirements.txt -r requirements-windows.txt -r requirements-build.txt"
+Invoke-Expression ".\$VENV\Scripts\pip.exe install cryptography"
 
 # install divio-cli
 Invoke-Expression ".\$VENV\Scripts\pip.exe install -e ."
@@ -26,6 +30,8 @@ md -Force binary
 
 # package app
 Invoke-Expression ".\$VENV\Scripts\pyinstaller.exe -F -y scripts\entrypoint.py -n divio-$ARCH.exe --hidden-import=_cffi_backend --distpath=binary"
+
+$ErrorActionPreference = "Stop"
 
 # run check
 Invoke-Expression ".\binary\divio-$ARCH version"
