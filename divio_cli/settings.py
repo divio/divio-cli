90f932422d65cae4d0f4272ad8e71deebba6fd95
Dennis Schwertel <dennisschwertel@gmail.com>
2020-07-07 17:06:51 +0200

    better global config name

--- divio_cli/settings.py
+++ divio_cli/settings.py
@@ -3,7 +3,7 @@ import os
 ACCESS_TOKEN_URL_PATH = "/account/desktop-app/access-token/"
 BOILERPLATE_CONFIG_FILENAME = "boilerplate.json"
 ADDON_CONFIG_FILENAME = "addon.json"
+DOCKER_TEST_IMAGE = "busybox:1.30"
 ALDRYN_DOT_FILE = ".aldryn"
 DIVIO_DOT_FILE = ".divio/config.json"
-DOCKER_TEST_IMAGE = "busybox:1.30"
-DIVIO_GLOBAL_CONFIG_FILE = os.path.join(os.getenv('XDG_CONFIG_HOME', os.path.expanduser("~/.config")), "divio.json")
+DIVIO_GLOBAL_CONFIG_FILE = os.path.join(os.getenv('XDG_CONFIG_HOME', os.path.expanduser("~/.config")), "divio/config.json")
