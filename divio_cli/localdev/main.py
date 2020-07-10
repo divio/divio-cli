43061befe52b36f4dc8f230e606104f63d60f2b5
Dennis Schwertel <dennisschwertel@gmail.com>
2020-07-10 14:08:54 +0200

    removed wrong line

--- divio_cli/localdev/main.py
+++ divio_cli/localdev/main.py
@@ -74,8 +74,6 @@ def configure_project(website_slug, path, client):
         path = os.path.join(path, settings.ALDRYN_DOT_FILE)
     else:
         path = os.path.join(path, settings.DIVIO_DOT_FILE)
-    path = os.path.join(path, settings.DIVIO_DOT_FILE)
-
 
     # Create folders if they don't exist yet.
     if not os.path.exists(os.path.dirname(path)):
