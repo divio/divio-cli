29ff1d2c84803e94d3fe1b0ab31962edc4451cd5
Dennis Schwertel <dennisschwertel@gmail.com>
2020-12-21 17:06:55 +0100

    lint

--- divio_cli/__init__.py
+++ divio_cli/__init__.py
@@ -2,5 +2,5 @@ try:
     from divio_cli.version import __version__
 except ModuleNotFoundError:
     from setuptools_scm import get_version
-    __version__ = get_version(root='..', relative_to=__file__)
 
+    __version__ = get_version(root="..", relative_to=__file__)
