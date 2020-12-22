572b237da2160aa01a4d641a0f700cff124753a1
Dennis Schwertel <dennisschwertel@gmail.com>
2020-12-22 12:57:36 +0100

    rewrite integration test logic

--- divio_cli/test/conftest.py
+++ divio_cli/test/conftest.py
@@ -4,40 +4,24 @@ import subprocess
 import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption("--testprojectname", action="store")
-    parser.addoption(
-        "--testprojectdirectory", action="store", default="test/data"
-    )
-
-
 @pytest.fixture(scope="session")
 def divio_project(request):
-    """
-    This fixture requires the `--test_project_name` command line parameter to
-    checkout an actual project for the integration test.
 
-    We try to setup up the first time and will not set it up again if it
-    already exists.
-    """
-    test_project_name = getattr(
-        request.config.option, "testprojectname", None
-    )
+
+    
+    test_project_name = os.getenv("TEST_PROJECT_NAME", None) 
     if test_project_name is None:
         pytest.skip(
-            "project name for the test is not supplied. Please use `--testprojectname <name>` to specify one ."
+            "project name for the test is not supplied. Please use $TEST_PROJECT_NAME to specify one."
         )
 
-    test_project_directory = request.config.option.test_project_directory
-
-    test_project_dir_full_path = os.path.join(
-        test_project_directory, test_project_name
-    )
+    test_project_dir_full_path = os.path.join("/test", test_project_name)
 
     if not os.path.exists(test_project_dir_full_path):
+        os.mkdir("/test")
         process = subprocess.Popen(
             ["divio", "project", "setup", test_project_name],
-            cwd=test_project_directory,
+            cwd="/test",
         )
         stdout, stderr = process.communicate()
     return test_project_dir_full_path
