8e5d9554e8d6016bf08faabcc5b4df3d6fbd4aff
Dennis Schwertel <dennisschwertel@gmail.com>
2020-12-23 11:00:21 +0100

    refactored tempdir logic

--- divio_cli/test/conftest.py
+++ divio_cli/test/conftest.py
@@ -6,7 +6,7 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def divio_project(request):
+def divio_project(request, tmpdir_factory):
 
     test_project_name = os.getenv("TEST_PROJECT_NAME", None)
     if test_project_name is None:
@@ -14,14 +14,12 @@ def divio_project(request):
             "project name for the test is not supplied. Please use $TEST_PROJECT_NAME to specify one."
         )
 
-    from tempfile import TemporaryDirectory
-
-    with TemporaryDirectory() as tmp_folder:
-        process = subprocess.Popen(
-            ["divio", "project", "setup", test_project_name],
-            cwd=tmp_folder,
-        )
-        stdout, stderr = process.communicate()
-        click.echo(stdout)
-        click.echo(stderr, err=True)
-        yield os.path.join(tmp_folder, test_project_name)
+    tmp_folder = tmpdir_factory.mktemp("data")
+    process = subprocess.Popen(
+        ["divio", "project", "setup", test_project_name],
+        cwd=str(tmp_folder),
+    )
+    stdout, stderr = process.communicate()
+    click.echo(stdout)
+    click.echo(stderr, err=True)
+    return os.path.join(tmp_folder, test_project_name)
