330fe6090379a5505dfc27db217b2376625d9cea
Dennis Schwertel <dennisschwertel@gmail.com>
2021-01-07 16:25:01 +0100

    lint

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import sys
 import setuptools
 
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 if __name__ == "__main__":
     setuptools.setup(
         install_requires=requirements,
-        use_scm_version = {
+        use_scm_version={
             # write_to needs be duplicated in pyproject.coml for compatibility
             # reasons
             "write_to": "divio_cli/version.py"
-        }
+        },
     )
