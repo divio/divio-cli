043a5b6ccea0d0e51700df8cf8e86e66dbc75fac
Florian Scherf <florian.scherf@divio.ch>
2023-09-06 13:46:24 +0200

    tests: _divio_project: disable stdout capturing during setup command; add more debug output
    When running the integration tests, the test suite has to setup a fully
    populated divio project, and the success of this stage is crucial for the tests
    to succeed. Therefore, the console output of the setup command is helpful for
    debugging.
    
    Previously, you could only switch on console output globally, which gets hard
    to read since the following tests produce output too.
    
    This patch disables pytests output capturing during the setup stage, adds some
    more debug prints, and re enables it afterwards.
    
    Signed-off-by: Florian Scherf <florian.scherf@divio.ch>

--- divio_cli/test/conftest.py
+++ divio_cli/test/conftest.py
@@ -13,7 +13,8 @@ TEST_DATA_DIRECTORY = "test_data"
 
 
 @pytest.fixture(scope="session")
-def _divio_project(request, tmpdir_factory):  # noqa: PT005
+def _divio_project(request, pytestconfig, tmpdir_factory):  # noqa: PT005
+    capturemanager = pytestconfig.pluginmanager.getplugin("capturemanager")
 
     # check if test project is set
     test_project_name = os.getenv("TEST_PROJECT_NAME", "")
@@ -27,38 +28,57 @@ def _divio_project(request, tmpdir_factory):  # noqa: PT005
     if not os.path.exists(TEST_DATA_DIRECTORY):
         os.makedirs(TEST_DATA_DIRECTORY)
 
-    # We can not use a fully randomized name as it normally would be a best
-    # practice. This path needs to be well known and static as we have to
-    # reference it in our test project to make docker-in-docker on Gitlab
-    # work with the right volume mounts and correct paths.
-    test_project_directory = os.path.join(
-        TEST_DATA_DIRECTORY, test_project_name
-    )
+    # setup divio project
+    try:
 
-    # Locally, we may run the tests multiple times
-    if os.path.exists(test_project_directory):
-        if os.getenv("TEST_KEEP_PROJECT", "0") == "1":
+        # disable stdout capturing
+        capturemanager.suspend_global_capture(in_=True)
+
+        print("\n=== divio project setup ===")  # noqa: T201
+
+        # We can not use a fully randomized name as it normally would be a best
+        # practice. This path needs to be well known and static as we have to
+        # reference it in our test project to make docker-in-docker on Gitlab
+        # work with the right volume mounts and correct paths.
+        test_project_directory = os.path.join(
+            TEST_DATA_DIRECTORY, test_project_name
+        )
+
+        # Locally, we may run the tests multiple times
+        if os.path.exists(test_project_directory):
+
             # Reuse the existing project
-            return test_project_directory
-        # Cleanup
-        shutil.rmtree(test_project_directory)
+            if os.getenv("TEST_KEEP_PROJECT", "0") == "1":
+                print("TEST_KEEP_PROJECT is set. skipping setup")  # noqa: T201
 
-    # setup
-    setup_command = f"divio app setup {test_project_name}"
-    env = os.environ.copy()
+                return test_project_directory
 
-    if "TEST_ZONE" in env:
-        env["DIVIO_ZONE"] = env["TEST_ZONE"]
+            # Cleanup
+            print(f"removing {test_project_directory}")  # noqa: T201
 
-    print(f"setup command: {setup_command}")  # noqa: T201
+            shutil.rmtree(test_project_directory)
 
-    subprocess.check_call(
-        shlex.split(setup_command),
-        env=env,
-        cwd=TEST_DATA_DIRECTORY,
-    )
+        # setup
+        setup_command = f"divio app setup {test_project_name}"
+        env = os.environ.copy()
 
-    return test_project_directory
+        if "TEST_ZONE" in env:
+            env["DIVIO_ZONE"] = env["TEST_ZONE"]
+
+        print(f"setup command: {setup_command}")  # noqa: T201
+
+        subprocess.check_call(
+            shlex.split(setup_command),
+            env=env,
+            cwd=TEST_DATA_DIRECTORY,
+        )
+
+        return test_project_directory
+
+    finally:
+        print("=== divio project setup finished ===")  # noqa: T201
+
+        capturemanager.resume_global_capture()
 
 
 @pytest.fixture(scope="session")
