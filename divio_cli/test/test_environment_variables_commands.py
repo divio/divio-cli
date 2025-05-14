83ac17635917eda95d136b18d7025a29b8ded2e1
Florian Scherf <florian.scherf@divio.ch>
2025-05-14 16:35:33 +0200

    fix: fix incompatibility with click>=8.2
    click 8.2 changed the behavior of `no_args_is_help` to return to shell with
    an exit code of 2 instead of 0, which broke some of our tests.
    
    This patch makes the tests aware of the new exit code.
    
    Signed-off-by: Florian Scherf <florian.scherf@divio.ch>

--- divio_cli/test/test_environment_variables_commands.py
+++ divio_cli/test/test_environment_variables_commands.py
@@ -9,27 +9,33 @@ TEST_PROJECT_ID = os.getenv("TEST_PROJECT_ID", None)
 TEST_PROJECT_UUID = os.getenv("TEST_PROJECT_UUID", None)
 
 ENVIRONMENT_VARIABLES_COMMANDS = [
-    "app environment-variables",
-    "app env-vars list",
-    f"app env-vars list --remote-id {TEST_PROJECT_ID}",
-    f"app env-vars list --remote-id {TEST_PROJECT_UUID}",
-    "app env-vars list -e live",
-    "app env-vars list --all-envs",
-    "app env-vars -p list",
-    "app env-vars list --limit 1",
-    "app env-vars --json list",
-    "app env-vars --txt list",
-    "app env-vars get SIMPLE_VAR",
-    "app env-vars get SIMPLE_VAR --all-envs",
-    "app env-vars get SIMPLE_VAR --all-envs --limit 1",
-    "app env-vars --json get SIMPLE_VAR",
-    "app env-vars --txt get SIMPLE_VAR",
+    (2, "app environment-variables"),
+    (0, "app env-vars list"),
+    (0, f"app env-vars list --remote-id {TEST_PROJECT_ID}"),
+    (0, f"app env-vars list --remote-id {TEST_PROJECT_UUID}"),
+    (0, "app env-vars list -e live"),
+    (0, "app env-vars list --all-envs"),
+    (0, "app env-vars -p list"),
+    (0, "app env-vars list --limit 1"),
+    (0, "app env-vars --json list"),
+    (0, "app env-vars --txt list"),
+    (0, "app env-vars get SIMPLE_VAR"),
+    (0, "app env-vars get SIMPLE_VAR --all-envs"),
+    (0, "app env-vars get SIMPLE_VAR --all-envs --limit 1"),
+    (0, "app env-vars --json get SIMPLE_VAR"),
+    (0, "app env-vars --txt get SIMPLE_VAR"),
 ]
 
 
 @pytest.mark.integration()
 @pytest.mark.parametrize("command", ENVIRONMENT_VARIABLES_COMMANDS)
 def test_call_click_commands(divio_project, command):
-    exitcode = subprocess.check_call(["divio", *shlex.split(command)])
+    expected_exitcode, command = command
 
-    assert exitcode == 0
+    if expected_exitcode == 2:
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_call(["divio", *shlex.split(command)])
+
+    else:
+        exitcode = subprocess.check_call(["divio", *shlex.split(command)])
+        assert exitcode == expected_exitcode
